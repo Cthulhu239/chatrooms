9b1732063b3b0724b511eea904957f7ede44409f
Kushan <kushangajbe73@gmail.com>
2026-05-07 16:51:34 +0530

    Change database password to empty string
    Updated database password to an empty string for security.

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'chat_db',
         'USER' : 'postgres',
-        'PASSWORD':'1234',
+        'PASSWORD':'',
         'HOST':'127.0.0.1',
         'PORT':'5433'
     }
